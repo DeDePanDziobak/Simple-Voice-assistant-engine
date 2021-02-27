04308d982c1ba39e4ddebcd73e0ae35f41e51dba
DeDePanDziobak <76704890+DeDePanDziobak@users.noreply.github.com>
2021-02-27 20:27:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,61 @@
 import speech_recognition as sr
 import pyttsx3 as tts
+import os, sys, time
 
+# Obiekty
 r = sr.Recognizer()
 engine = tts.init()
-engine.setProperty('rate', 150)
+engine.setProperty('voice', engine.getProperty('voices')[0].id)
 
-def mow(text):
+# Wczytać firefoxa
+firefox = 'C:\\"Program Files"\\"Mozilla Firefox"\\firefox.exe'
+
+def speak(text):
 	engine.say(text)
 	engine.runAndWait()
 
 def getText():
-	with sr.Microphone() as source:
-		try:
-			print("Słucham...")
+	try:
+		with sr.Microphone() as source:
+			print("Nasłuchuję...", end='\r')
 			audio = r.listen(source)
-			text = r.recognize_google(audio, language='pl-PL')
-			if text != "":
+			text = r.recognize_google(audio, language="pl-PL")
+			if text == "":
+				return None
+			else:
 				return text
-			return 0
-		except:
-			return 0
+	except:
+		return None
 
+def czy_zawiera(string, slowa):
+	return [element for element in slowa if element in string.lower()]
+
+#	for element in slowa:
+#		if element in string.lower():
+#			lista.append(element)
+# return lista
+
+# Stałe listy gestów
+WYKRYJ = ['bot', 'robot', 'robocie']
+DOWIDZENIA = ['dowidzenia', 'do widzenia', 'papa', 'żegnaj']
+SZUKAJ = ['wyszukaj', 'szukaj', 'znajdź', 'google', 'googluj', 'pokaż']
+
+print("Aby wyjść powiedz 'Dowidzenia'")
 while True:
-	txt = getText()
-	if not txt == 0:
-		print(txt)
-		mow(txt)
-		break
-	else:
-		print("Nie udało się rozpoznać...")
-		continue
+	time.sleep(0.5)
+	cur = getText()
+	print(cur)
+	print(" "*50, end="\r")
+	if cur != None:
+		if len(czy_zawiera(cur, WYKRYJ)):
+			if len(czy_zawiera(cur, DOWIDZENIA)):
+				speak("Żegnaj.")
+				break
+			elif len(czy_zawiera(cur, SZUKAJ)):
+				linczek = cur.lower().split(' ' + czy_zawiera(cur, SZUKAJ)[0] + ' ')[1]
+				speak("Oto co udało mi się znaleźć.")
+				url = "https://www.google.com/search?q=" + linczek.replace(" ", "+").replace("?", "%3F")
+				os.system(firefox + " " + url)
+
+				# www.google.pl/search?q=jak+zrobić+naleśniki
+				# 'Robocie', 'jak zrobić nalesniki'
